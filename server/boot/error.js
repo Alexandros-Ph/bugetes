b03b2eeb9e00dcc8d849ebb503a0b0b897d6438b
lykmast <lykmast@gmail.com>
2019-03-01 00:55:21 +0200

    some custom error handling for rest (according to specs)

---
+++ server/boot/error.js
@@ -0,0 +1,28 @@
+module.exports = function(app) {
+
+	var remotes = app.remotes();
+
+	remotes.options.rest = remotes.options.rest || {};
+	remotes.options.rest.handleErrors = false;
+	app.middleware('final', FinalErrorHandler);
+
+	function FinalErrorHandler(err, req, res, next) {
+		if (err) {
+			if (err.statusCode == 422) {
+				res.status(400).send({
+					statusCode: 400,
+					data: {},
+					error: {
+						statusCode: 400,
+						name: 'Bad Request',
+						message: err.message,
+					},
+				}).end();
+			}
+			else{
+				res.status(err.statusCode).send(err).end();
+			}
+		}
+		next();
+	}
+};
